c8647206f449fbc9942f7e5ec18d3ec489bf7c14
aleynaayvaz <anyelayoszo@gmail.com>
2026-02-21 02:54:50 +0300

    React todo: görev ekleme, silme, geri alma ve düzenleme modu eklendi

--- kariyer-yolculugum/src/App.jsx
+++ kariyer-yolculugum/src/App.jsx
@@ -5,12 +5,37 @@ import './App.css'
 
 function App() {
  const [gorevler, setGorevler] = useState([])
-  const [inputDegeri, setInputDegeri] = useState("")
+ const [inputDegeri, setInputDegeri] = useState("")
+ const [silinenler, setSilinenler] = useState([])
+ const [duzenlenecekIndex, setDuzenlenecekIndex] = useState(null)
+ const [duzenleniyorMu, setDuzenleniyorMu] = useState(false)
+ const [duzenlenecekMetin, setDuzenlenecekMetin] = useState("")
+  
 
-  function gorevEkle() {
+function gorevEkle() {
     if (inputDegeri.trim() === "") return
     setGorevler([...gorevler, inputDegeri])
     setInputDegeri("")
+  }
+
+  function gorevSil(index) {
+    const yeniListe = gorevler.filter((_, i) => i !== index)
+    console.log(yeniListe, "yeniListe")
+    setGorevler(yeniListe)
+    setSilinenler([...silinenler, gorevler[index]])
+    console.log(silinenler, "silinenler")
+  }
+
+  function silineniGeriAl () {
+    setGorevler([...gorevler, silinenler[silinenler.length-1]])
+    setSilinenler([])
+    console.log(silinenler, "silineniGeriAl")
+  }
+
+  function gorevDuzenle (index) {
+    setDuzenlenecekIndex(index)
+    setDuzenleniyorMu(true)
+    setDuzenlenecekMetin(gorevler[index])
   }
 
 
@@ -25,9 +50,18 @@ function App() {
       <button onClick={gorevEkle}>
         Ekle
       </button>
+      {silinenler.length > 0 && <button onClick={() => silineniGeriAl(silinenler.length-1)}>Geri Al</button>}    
       <ul>
         {gorevler.map((gorev, index) => (
-          <li key={index}>{gorev}</li>
+          <li key={index}> {index === duzenlenecekIndex ? 
+          <input
+            value={duzenlenecekMetin}
+            onChange={(e) => setDuzenlenecekMetin(e.target.value)}
+          /> : 
+          <span>{gorev}</span> }
+          <button onClick={() => gorevDuzenle(index)}>Düzenle</button>
+          <button onClick={() => gorevSil(index)}>Sil</button>
+          </li>
         ))}
       </ul>
     </div>
